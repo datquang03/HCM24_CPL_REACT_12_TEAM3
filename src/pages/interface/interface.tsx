197f76cf6e1e414834f7d3f4b0d569a10f77b045
anle4coding <lethuan.esther@gmail.com>
2024-09-17 17:20:00 +0700

    An-Type Annotations, Interface, Type

--- src/pages/interface/interface.tsx
+++ src/pages/interface/interface.tsx
@@ -1,7 +1,139 @@
-import React from "react";
+import React, { useState } from "react";
+import typeImage from '/src/assets/typeImage.jpg';
+import interfaceImage from '/src/assets/interfaceImage.jpg';
+import typeMerge from '/src/assets/typeMerge.jpg';
+import interfaceMerge from '/src/assets/interfaceMerge.jpg';
 
-const InterfacePage = () => {
-  return <div>InterfacePage</div>;
+interface Person {
+  name: string;
+  age: number;
+  active: boolean;
+}
+const InterfacePage: React.FC = () => {
+  const [name, setName] = useState<string>(""); // Chuỗi trống ban đầu
+  const [age, setAge] = useState<number | string>(0); // Số 0 ban đầu, sử dụng kiểu number hoặc string
+  const [active, setActive] = useState<boolean>(false); // Trạng thái ban đầu là false
+  const [person, setPerson] = useState<Person | null>(null); // Biến person có thể là kiểu Person hoặc null
+
+  // Hàm xử lý khi submit form
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    // Chuyển giá trị `age` thành 0 nếu người dùng chưa nhập
+    const parsedAge = age === "" ? 0 : Number(age);
+
+    const newPerson: Person = {
+      name,
+      age: parsedAge,
+      active,
+    };
+
+    setPerson(newPerson);
+  };
+
+  // Hàm xử lý khi input "Age" được focus
+  const handleFocus = () => {
+    if (age === 0) {
+      setAge("");
+    }
+  };
+
+  return (
+    <div>
+      <h1 className="text-2xl font-bold mb-6 text-center text-black-600">Interface and Type</h1>
+      <p className="text-lg font-medium text-gray-700 mb-4">
+        Typescript has 3 main types:
+      </p>
+      <ul className="list-disc list-inside pl-4 space-y-2 text-gray-600">
+        <li>Any kind</li>
+        <li>Integrated type</li>
+        <li>User-defined type</li></ul>
+      <ul className=" list-inside mb-4 text-red-500">
+        <li className="font-bold">Interfaces and Types fall under the 3rd category - User-Defined Types.</li>
+      </ul>
+
+      <p className="text-lg font-medium text-gray-700 mb-4">
+        Declare
+      </p>
+      <img src={typeImage} alt="type" className="mx-auto mb-4" />
+      <img src={interfaceImage} alt="interface" className="mx-auto mb-4" />
+
+      <p className="text-lg font-medium text-gray-700 mb-4">
+        Interfaces can be merged while Types cannot
+      </p>
+      <img src={interfaceMerge} alt="interface" className="mx-auto mb-4" />
+      <img src={typeMerge} alt="type" className="mx-auto mb-4" />
+      <ul className=" list-inside mb-4 text-red-500">
+        <li className="font-bold">Error : Duplicate identifier 'Shape'.</li>
+      </ul>
+
+
+      <div className="min-h-screen bg-gray-100 flex items-center justify-center p-4">
+        <div className="bg-white p-8 rounded-lg shadow-lg max-w-md w-full">
+          <h1 className="text-2xl font-bold mb-6 text-center text-blue-600">Person Information</h1>
+
+          {/* Form nhập liệu */}
+          <form onSubmit={handleSubmit}>
+            <div className="mb-4">
+              <label htmlFor="name" className="block text-gray-700 font-semibold mb-2">Name: </label>
+              <input
+                type="text"
+                id="name"
+                className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-400"
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+                required
+              />
+            </div>
+
+            <div className="mb-4">
+              <label htmlFor="age" className="block text-gray-700 font-semibold mb-2">Age: </label>
+              <input
+                type="number"
+                id="age"
+                className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-400"
+                value={age}
+                onFocus={handleFocus} // Xử lý khi người dùng trỏ chuột vào
+                onChange={(e) => setAge(e.target.value)} // Chuyển đổi giá trị thành chuỗi
+                required
+              />
+            </div>
+
+            <div className="mb-6">
+              <label htmlFor="active" className="block text-gray-700 font-semibold mb-2">Active: </label>
+              <div className="flex items-center">
+                <input
+                  type="checkbox"
+                  id="active"
+                  className="h-4 w-4 text-blue-600 focus:ring-blue-500 border-gray-300 rounded"
+                  checked={active}
+                  onChange={(e) => setActive(e.target.checked)}
+                />
+                <label htmlFor="active" className="ml-2 text-gray-600">Active User</label>
+              </div>
+            </div>
+
+            <button
+              type="submit"
+              className="w-full bg-blue-500 text-white font-semibold py-2 px-4 rounded hover:bg-blue-600 transition duration-300"
+            >
+              Submit
+            </button>
+          </form>
+
+          {/* Hiển thị kết quả sau khi người dùng nhập */}
+          {person && (
+            <div className="mt-8 bg-gray-50 p-4 rounded-md shadow-sm">
+              <h2 className="text-xl font-semibold text-gray-800 mb-4">Person Information</h2>
+              <p><strong>Name:</strong> {person.name}</p>
+              <p><strong>Age:</strong> {person.age}</p>
+              <p><strong>Active:</strong> {person.active ? "Yes" : "No"}</p>
+            </div>
+          )}
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default InterfacePage;
--- src/pages/typeAnnotation/typeAnnotation.tsx
+++ src/pages/typeAnnotation/typeAnnotation.tsx
@@ -1,7 +1,54 @@
 import React from "react";
 
-const TypeAnnotationPage = () => {
-  return <div>TypeAnnotationPage</div>;
-};
+const TypeAnnotationPage: React.FC = () => {
+  // Variables
+  let counter: number = 1; // Khai báo biến `counter` với kiểu number
 
+
+  let name: string = 'John'; // Biến `name` có kiểu string
+  let age: number = 25; // Biến `age` có kiểu number
+  let active: boolean = true; // Biến `active` có kiểu boolean
+
+  // Arrays
+  let names: string[] = ['John', 'Jane', 'Peter', 'David', 'Mary']; // Mảng chuỗi
+  let numbers: number[] = [1, 2, 3, 4, 5]; // Mảng số
+
+  // Objects
+  let person: {
+    name: string;
+    age: number
+  };
+
+  person = {
+    name: 'John',
+    age: 25
+  }; // valid
+
+
+  // Functions
+  let greeting: (name: string) => string; // Hàm nhận vào tham số name (string) và trả về chuỗi
+  greeting = function (name: string) {
+    return `Hi ${name}`;
+  };
+
+  // Gọi hàm greeting
+  const greetMessage = greeting("Peter");
+
+  return (
+    <div>
+      <h1 className="text-2xl font-bold mb-6 text-center text-black-600">Type Annotations in TypeScript</h1>
+      <p className="text-lg font-medium text-gray-700 mb-4 pl-2">
+        TypeScript uses type annotations to explicitly specify data types for identifiers such as variables, functions, objects, etc.
+      </p>
+      <div className="border border-gray-400 p-4 rounded mb-4 pl-4">
+        <p className="text-lg font-medium text-gray-700 mb-4 pl-4">Counter: {counter}</p>
+        <p className="text-lg font-medium text-gray-700 mb-4 pl-4">Name: {name}</p>
+        <p className="text-lg font-medium text-gray-700 mb-4 pl-4">Age: {age}</p>
+        <p className="text-lg font-medium text-gray-700 mb-4 pl-4">Active: {active ? 'Yes' : 'No'}</p>
+        <p className="text-lg font-medium text-gray-700 mb-4 pl-4">Names: {names.join(', ')}</p>
+        <p className="text-lg font-medium text-gray-700 mb-4 pl-4">Greeting: {greetMessage}</p>
+      </div>
+    </div>
+  );
+};
 export default TypeAnnotationPage;
