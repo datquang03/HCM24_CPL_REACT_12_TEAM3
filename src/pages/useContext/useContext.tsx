9ae5a7f4d478c7a82c9221be3c68af16a410d170
Anhsapper <anhntse173567@fpt.edu.vn>
2024-09-17 21:22:28 +0700

    update useContext

--- src/pages/useContext/useContext.tsx
+++ src/pages/useContext/useContext.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import { TodoProvider } from "./Todo/TodoContext";
 import TodoPage from "./Todo/TodoPage";
+import ContextConcept from "./ContextConcept";
 
 const UseContextPage = () => {
   return <div>
     <TodoProvider>
+      <ContextConcept/>
       <TodoPage/>
     </TodoProvider>
   </div>;
